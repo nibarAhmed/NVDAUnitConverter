d5ae5d2e65c2409bb4b14c017487206a627caa72
nibarsito <nibarsito2@gmail.com>
2023-01-15 15:17:11 +0100

    added enums to store units

---
+++ Unit.py
@@ -0,0 +1,5 @@
+from enum import Enum
+class Unit(Enum):
+    #this enum is used to store units. Each unit will also have diferent values that indicates from what they can be converted to.
+    farenheightToCelsius=1
+    celsiusToFarenheight=2
